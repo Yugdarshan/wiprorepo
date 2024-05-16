6a6c6ca8fa4df3038c36ec95343d9ae383618e22
Yugdarshan <66959020+Yugdarshan@users.noreply.github.com>
2024-05-16 14:39:54 +0530

    Update File1.java added variable x

--- File1.java
+++ File1.java
@@ -2,6 +2,7 @@ import java.lang.*;
 
 public class File1{
 	public static void main(String args[]){
+		int x = 100;
 		System.out.println("Hello world");
 		System.out.println("This Is Java Program");
 		
